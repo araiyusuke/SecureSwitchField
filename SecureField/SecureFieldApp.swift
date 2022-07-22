455c918a23a101306ffc374d7c1b89868163112c
araiyusuke <info.yusuke.arai@gmail.com>
2022-07-22 22:04:57 +0900

    Initial Commit

---
+++ SecureField/SecureFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureFieldApp.swift
+//  SecureField
+//
+//  Created by 名前なし on 2022/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SecureFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
